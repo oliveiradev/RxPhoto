024481b5d462bfa87f8eec70b2703194e6d5fdc7
oliveiradev <felipe.oliveiradev@gmail.com>
2016-11-10 21:24:03 -0300

    add new constant

--- lib/src/main/java/com/github/oliveiradev/lib/shared/Constants.java
+++ lib/src/main/java/com/github/oliveiradev/lib/shared/Constants.java
@@ -6,4 +6,5 @@ package com.github.oliveiradev.lib.shared;
 public class Constants {
     public static int REQUEST_CODE_ATTACH_IMAGE = 9123;
     public static int REQUEST_CODE_TAKE_PICURE = 9124;
+    public static String REQUEST_TYPE_EXTRA = "request_type_extra";
 }
